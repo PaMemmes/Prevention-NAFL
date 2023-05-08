3924dce2be48b78214a96e80c2466ddcb31bfed4
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-05-08 13:49:48 +0200

    Add force plot per class

--- src/interpret.py
+++ src/interpret.py
@@ -11,17 +11,24 @@ def interpret_tree(model, train, test, df_cols):
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(test_df)
 
-    #f = shap.force_plot(explainer.expected_value, shap_values, test, feature_names=df_cols, show=False)
-    #shap.save_html('../results/force_plot.htm', f)
-    #plt.close()
-    #print('After force plot')
+    f = shap.force_plot(explainer.expected_value[0], shap_values[0], test, feature_names=df_cols, show=False)
+    shap.save_html('../results/force_plot_0.htm', f)
+    plt.close()
+    
+    f = shap.force_plot(explainer.expected_value[1], shap_values[1], test, feature_names=df_cols, show=False)
+    shap.save_html('../results/force_plot_1.htm', f)
+    plt.close()
+
+    f = shap.force_plot(explainer.expected_value[2], shap_values[2], test, feature_names=df_cols, show=False)
+    shap.save_html('../results/force_plot_2.htm', f)
+    plt.close()
+
+    f = shap.force_plot(explainer.expected_value[3], shap_values[3], test, feature_names=df_cols, show=False)
+    shap.save_html('../results/force_plot_3.htm', f)
+    plt.close()
+
     shap.summary_plot(shap_values, test, plot_type="bar", feature_names=df_cols, show=False)
     f = plt.gcf()
     f.savefig('../results/summary_bar.png', bbox_inches='tight', dpi=300)
-    plt.close()
-    print('After first summary plot')
-    shap.summary_plot(shap_values, test, feature_names=df_cols, show=False)
-    f = plt.gcf()
-    f.savefig('../results/summary.png', bbox_inches='tight', dpi=300)
     plt.close()
     print('End interpreting...')
