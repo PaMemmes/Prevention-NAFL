a40f4ac1d5b9f7317985c30418a6b5804bff5f57
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-05-10 22:01:53 +0200

    Move functions

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,41 +1,10 @@
 import numpy as np
 import pandas as pd
 from ydata_profiling import ProfileReport
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, OrdinalEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 
-def remove_nan(x, y):
-    indices_to_keep = ~y.isin([np.nan,np.inf, -np.inf]).any(axis=1)
-    len_y = len(y)
-    x = x.loc[indices_to_keep]
-    y = y.loc[indices_to_keep]
-    print('Dropped rows (=Rows that have missing "Stage"): ', len_y - len(y))
-    print('Perc dropped rows: ', 100 - (len(y) / len_y * 100))
-    return x, y
-
-def factorize(df, cols):
-    for col in cols:
-        df[col] = pd.factorize(df[col])[0] + 1
-    return df
-
-def one_hot_encoding(df, cols, cardinality):
-    low_cardinality_cols = [col for col in cols if df[col].nunique() < cardinality]
-    oh_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
-    oh_df = pd.DataFrame(oh_encoder.fit_transform(df[low_cardinality_cols]))
-    oh_df.index = df.index
-    oh_df.columns = oh_encoder.get_feature_names_out()
-    df = df.drop(low_cardinality_cols, axis=1)
-    df = pd.concat([df, oh_df], axis=1)
-
-    df.columns = df.columns.astype(str)
-    return df
-
-def get_categoricals(df):
-    num_cols = df._get_numeric_data().columns
-    
-    cats = list(set(df.columns) - set(num_cols))
-    return cats
+from utils.utils import remove_nan, one_hot_encoding, get_categoricals
 
 def preprocess(df):
     df = df.drop('ID', axis=1)
@@ -59,11 +28,11 @@ def preprocess(df):
     
     x, y = remove_nan(x, y)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=0.15, random_state=420)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=0.15, random_state=42)
 
-    scaler = MinMaxScaler()
-    x_train = scaler.fit_transform(x_train)
-    x_test = scaler.transform(x_test)
+    # scaler = MinMaxScaler()
+    # x_train = scaler.fit_transform(x_train)
+    # x_test = scaler.transform(x_test)
 
     return x_train, x_test, y_train.astype(int), y_test.astype(int), x.columns
 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,12 +1,13 @@
 from collections import defaultdict
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import cohen_kappa_score
 from sklearn.metrics import precision_recall_fscore_support as score
 from sklearn.metrics import classification_report
-
-from utils.plots import plot_confusion_matrix
 
 
 
@@ -25,6 +26,36 @@ def calc_all(model, x_test, y_test):
     print('Recall', recall)
     print('fscore:', fscore)
     print('Cohen kappa', cohen_kappa)
-    plot_confusion_matrix(cm, name='cm')
-
     return cm, cm_norm, preds
+
+def remove_y_nans(x, y):
+    indices_to_keep = ~y.isin([np.nan, np.inf, -np.inf]).any(axis=1)
+    len_y = len(y)
+    x = x.loc[indices_to_keep]
+    y = y.loc[indices_to_keep]
+    print('Dropped rows (=Rows that have missing "Stage"): ', len_y - len(y))
+    print('Perc dropped rows: ', 100 - (len(y) / len_y * 100))
+    return x, y
+
+def factorize(df, cols):
+    for col in cols:
+        df[col] = pd.factorize(df[col])[0] + 1
+    return df
+
+def one_hot_encoding(df, cols, cardinality):
+    low_cardinality_cols = [col for col in cols if df[col].nunique() <= cardinality]
+    oh_encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+    oh_df = pd.DataFrame(oh_encoder.fit_transform(df[low_cardinality_cols]))
+    oh_df.index = df.index
+    oh_df.columns = oh_encoder.get_feature_names_out()
+    df = df.drop(low_cardinality_cols, axis=1)
+    df = pd.concat([df, oh_df], axis=1)
+
+    df.columns = df.columns.astype(str)
+    return df
+
+def get_categoricals(df):
+    num_cols = df._get_numeric_data().columns
+    
+    cats = list(set(df.columns) - set(num_cols))
+    return cats
