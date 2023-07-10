383dc8fd14d9f87f130fade1594a2e14146a6af9
Pascal Memmesheimer <pascalmemmesheimer@gmail.com>
2023-07-11 00:04:52 +0200

    Add hyperparamoptim

--- src/ffn.py
+++ src/ffn.py
@@ -1,3 +1,7 @@
+import os
+from typing import List
+from typing import Optional
+
 from torch import optim, nn, utils, Tensor
 import torch
 import pytorch_lightning as pl
@@ -8,6 +12,9 @@ from torchmetrics.classification import MulticlassAccuracy
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning import loggers as pl_loggers
 
+import optuna
+from optuna.integration import PyTorchLightningPruningCallback
+
 import pandas as pd
 from sklearn.impute import SimpleImputer
 import numpy as np
@@ -15,72 +22,169 @@ import numpy as np
 from preprocess import preprocess
 from utils.utils import remove_y_nans, one_hot_encoding, get_categoricals
 
+MODEL_DIR = 'logs/'
+BATCH_SIZE = 8
+
+def objective(trial):    
+    n_layers = trial.suggest_int('n_layers', 1, 6)
+    dropout = trial.suggest_float('dropout', 0.1, 0.5)
+    output_dims = [
+        trial.suggest_int('n_units_l{}'.format(i), 4, 128, log=True) for i in range(n_layers)
+    ]
+
+    model = NeuralNetwork(21, dropout, output_dims)
+    datamodule = DataModule(batch_size=BATCH_SIZE)
+
+    trainer = pl.Trainer(
+        logger=True,
+        enable_checkpointing=True,
+        max_epochs=100,
+        accelerator='auto',
+        log_every_n_steps=10,
+        callbacks=PyTorchLightningPruningCallback(trial, monitor='train_loss')
+    )
+    trainer.fit(model, datamodule=datamodule)
+    return trainer.callback_metrics['val_acc'].item()
+
+def retrain_objective(trial):    
+    n_layers = trial.suggest_int('n_layers', 1, 3)
+    dropout = trial.suggest_float('dropout', 0.2, 0.5)
+    output_dims = [
+        trial.suggest_int('n_units_l{}'.format(i), 4, 128, log=True) for i in range(n_layers)
+    ]
+
+    model = NeuralNetwork(21, dropout, output_dims)
+    datamodule = DataModule(batch_size=16)
+
+    trainer = pl.Trainer(
+        logger=True,
+        enable_checkpointing=True,
+        max_epochs=100,
+        accelerator='auto',
+        log_every_n_steps=10,
+        callbacks=PyTorchLightningPruningCallback(trial, monitor='train_loss')
+    )
+    trainer.fit(model, datamodule=datamodule)
+    trainer.test(datamodule=datamodule)
+    return trainer.callback_metrics['test_acc'].item()
+
 class KaggleDataSet(Dataset):
     def __init__(self, x, y):
         x, y = x.values, y.values
         self.x = torch.tensor(x, dtype=torch.float32, requires_grad=True)
         self.y = torch.tensor(y,dtype=torch.long)
-  
+
     def __len__(self):
         return len(self.y)
-    
+
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
 
+class DataModule(pl.LightningModule):
+    def __init__(self, batch_size):
+        super().__init__()
+        self.batch_size = batch_size
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        df = pd.read_csv('../data/kaggle_cirrhosis.csv')
+        x_train, x_val, x_test, y_train, y_val, y_test = preprocess(df, nn=True)
+
+        self.train_set = KaggleDataSet(x_train, y_train)
+        self.val_set = KaggleDataSet(x_val, y_val)
+        self.test_set = KaggleDataSet(x_test, y_test)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.train_set, batch_size=self.batch_size)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.val_set, batch_size=self.batch_size)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.test_set, batch_size=self.batch_size)
+
+class Net(nn.Module):
+    def __init__(self, input_dim, dropout: float, output_dims: List[int]) -> None:
+        super().__init__()
+        layers: List[nn.Module] = []
+
+        for output_dim in output_dims:
+            layers.append(nn.Linear(input_dim, output_dim))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout))
+            input_dim = output_dim
+
+        layers.append(nn.Linear(input_dim, 4))
+        layers.append(nn.LogSoftmax(dim=1))
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, data: torch.Tensor) -> torch.Tensor:
+        logits = self.layers(data)
+
+        return F.log_softmax(logits, dim=1)
 
 class NeuralNetwork(pl.LightningModule):
-    def __init__(self, in_features):
+    def __init__(self, input_dim, dropout, output_dims):
         super().__init__()
-        self.layer1 = nn.Sequential(nn.Linear(in_features, 8), nn.ReLU(), nn.Linear(8, 10))
-        self.layer2 = nn.Sequential(nn.Linear(10, 15), nn.ReLU(), nn.Linear(15, 4))
+        self.model = Net(input_dim, dropout, output_dims)
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        return x
-    
+        return self.model(x)
+
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
         return optimizer
 
     def training_step(self, batch, batch_idx):
         x, y = batch
         y = y.squeeze(1)
-        x = self.forward(x)
-        train_loss = F.cross_entropy(x, y)
+        x = self(x)
+        train_loss = F.nll_loss(x, y)
+        self.log('train_loss', train_loss, prog_bar=True)
         return train_loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y = y.squeeze(1)
-        x = self.forward(x)
-        val_loss = F.cross_entropy(x, y)
+        x = self(x)
+        val_loss = F.nll_loss(x, y)
+        val_acc = MulticlassAccuracy(num_classes=4)
+        val_acc = val_acc(x, y)
         self.log('val_loss', val_loss, prog_bar=True)
+        self.log('val_acc', val_acc)
+        self.log('hp_metric', val_acc, on_step=False, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         y = y.squeeze(1)
         x = self.forward(x)
-        test_loss = F.cross_entropy(x, y)
+        test_loss = F.nll_loss(x, y)
         accuracy = MulticlassAccuracy(num_classes=4)
         accuracy = accuracy(x, y)
-        self.log_dict({'test_loss': test_loss, 'accuracy': accuracy}, prog_bar=True)
+        self.log_dict({'test_loss': test_loss, 'test_acc': accuracy}, prog_bar=True)
 
 if __name__ == '__main__':
-    df = pd.read_csv('../data/kaggle_cirrhosis.csv')
-    x_train, x_val, x_test, y_train, y_val, y_test = preprocess(df, nn=True)
 
-    train_set = KaggleDataSet(x_train, y_train)
-    val_set = KaggleDataSet(x_val, y_val)
-    test_set = KaggleDataSet(x_test, y_test)
+    pruner = optuna.pruners.MedianPruner()
+    study = optuna.create_study(direction='maximize', pruner=pruner)
+    study.optimize(objective, n_trials=100, timeout=600)
 
-    train_loader = DataLoader(train_set, batch_size=8)
-    val_loader = DataLoader(val_set, batch_size=8)
-    test_loader = DataLoader(test_set, batch_size=8)
+    print('Number of finished trials: {}'.format(len(study.trials)))
 
-    tb_logger = pl_loggers.TensorBoardLogger(save_dir="logs/")
+    print('Best trial:')
+    trial = study.best_trial
 
-    neural_network = NeuralNetwork(x_train.shape[1])
-    trainer = pl.Trainer(log_every_n_steps= 10, logger=tb_logger, callbacks=[EarlyStopping(monitor='val_loss', mode='min')])
-    trainer.fit(model=neural_network, train_dataloaders=train_loader, val_dataloaders=val_loader)
-    trainer.test(model=neural_network, dataloaders=test_loader)
+    print('  Value: {}'.format(trial.value))
+
+    print('  Params: ')
+    for key, value in trial.params.items():
+        print(f'    {key}: {value}')
+
+    fig = optuna.visualization.plot_optimization_history(study)
+    fig.show()
+    retrain_objective(study.best_trial)
+
+    # neural_network = NeuralNetwork(x_train.shape[1])
+    # trainer = pl.Trainer(log_every_n_steps= 10, logger=tb_logger, callbacks=[EarlyStopping(monitor='val_loss', mode='min')])
+    # trainer.fit(model=neural_network, train_dataloaders=train_loader, val_dataloaders=val_loader)
+    # trainer.test(model=neural_network, dataloaders=test_loader)
